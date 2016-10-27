2d3c73a80f224e4dd620f9d2b3322e2834a4ba76
Silverfeelin <Silverfeelin@gmail.com>
2016-10-27 04:05:58 +0200

    Comments removed. Some documentation improved.
    - Different interface, since this interface is meant for mcontroller.position().
    - Modify cursor when paused, instead of showing it on the interface. Is this possible?
    - Transparency support?
    - Double click support?
     - First click: queue task. Second click on same region: run other function.
    - Highlight using replace directives, and unique RGBA colors for each regions in the interface?

--- scripts/bindMenu.lua
+++ scripts/bindMenu.lua
@@ -1,7 +1,5 @@
 require "/scripts/keybinds.lua"
 
--- I think i removed all of my personal customizations, but it's possible i missed something. Sorry!
-
 bm = {}
 bm.config = root.assetJson("/scripts/bindMenu.config")
 
@@ -22,12 +20,6 @@ for i=1,bm.config.regions[1] do
     bm.regions[i][j] = {}
   end
 end
-
---[[
- I set this and the config back to the old image(plus the pixel icon for the text function) just because it's my preference.
- I personally find the new image is far too large, ugly(sorry), and takes up too much space on-screen.
- I was going to make a new image since this was meant for mcontroller position and now its tech pos, but i thought of a new way to make it work through animations, so i'm going to test that first.
-]]--
 
 bm.imageSize = root.imageSize("/interface/streamingvideo/icon.png")
 bm.imageBlockSize = { bm.imageSize[1] / 8, bm.imageSize[2] / 8 }
@@ -57,10 +49,7 @@ end
 
 function bm.getProjectile(pos, showPause)
   local dir = ""
-  -- Possible to turn the cursor into the pause icon using a similar method? Unsure how the cursor works.
-  -- Maybe take a look at how picking items and such works. Rather than replacing the cursor, it appears alongside it.
   if showPause ~= true then
-    -- If not making the image slightly transparent during normal use, while paused could also be useful.
     dir = "?replace;000000fe=00000000;463818fe=00000000;6d6330fe=00000000;251c0bfe=00000000;c0cecefe=00000000;edf9f9fe=00000000;d8c4a1fe=00000000;ae946ffe=00000000;705c43fe=00000000"
   end
 
@@ -80,13 +69,10 @@ function bm.getProjectile(pos, showPause)
               action = "particle",
               specification = {
                 type = "textured",
-                -- Maybe using a new image, use the replace directive to make the image slightly transparent, so you can see things behind it.
-                -- This would also allow bigger images without everything in the background being blocked out
                 image = "/interface/streamingvideo/icon.png?replace;ff00f6=00000000" .. dir,
                 layer = "front",
                 initial = "drift",
                 flippable = false,
-                -- Set to fullbright so it doesn't turn practically unusable while in a dark area. God that's irritating.
                 fullbright = true,
                 size = 1,
                 light = {0, 0, 0},
@@ -113,14 +99,18 @@ end
   Binds a function to the region at [x,y]. Multiple functions can be bound to the same region.
   @param x - Horizontal region index (left to right). Should be between 1 and bm.config.regions[1].
   @param y - Vertical region index (bottom to top). Should be between 1 and bm.config.regions[2].
-  @param config - Table to run through. See bind examples @ line ~315
+  @param config - Bind arguments. Expected format:
+    { primary = { func = function, enabled = boolean }, alt = { func = function, enabled = boolean } }
+    You can leave out `enabled` (defaults to true). You can leave out `alt` or `primary` entirely.
+    This allows you to bind a function to the left mouse button, right mouse button or both.
+    An example configuration can be found near the bottom of the script.
 ]]
 function bm.bind(x, y, config)
   if not bm.regions[x] or not bm.regions[x][y] then error("") return end
-  if not config or type(config) ~= "table" then error("bm.bind - config for bm.bind not defined/invalid") return end
+  if not config or type(config) ~= "table" then error("`config` is invalid/undefined.") return end
   -- func checks
-  if config.primary and type(config.primary.func) ~= "function" then error("bm.bind - func in primary is invalid/undefined") return end
-  if config.alt and type(config.alt.func) ~= "function" then error("bm.bind - func in alt is invalid/undefined") return end
+  if config.primary and type(config.primary.func) ~= "function" then error("`func` in `config.primary` is invalid/undefined.") return end
+  if config.alt and type(config.alt.func) ~= "function" then error("`func` in `config.alt` is invalid/undefined.") return end
   -- enabled checks
   if config.primary and type(config.primary.enabled) ~= "boolean" then config.primary.enabled = true end
   if config.alt and type(config.alt.enabled) ~= "boolean" then config.alt.enabled = true end
@@ -149,8 +139,6 @@ end
   @see bm.getRegion
 ]]
 function bm.activate(rclick)
-  -- if rclick isn't defined set it to false.
-
   if bm.queued() then
     -- If queued and right click, cancel queue.
     if rclick then
@@ -176,9 +164,6 @@ function bm.activate(rclick)
   end
 end
 
--- Instead of a right clicking system for more(practical) functions on one region, there's also potential for a double click system.
--- For example, when queueing, if another click is done on the same region, start another queue that will run your 2nd function.
--- If second click is NOT made in the same region, continue with the first function.
 function bm.queue(callback)
   table.insert(bm.queueCallbacks, callback)
 end
@@ -233,8 +218,6 @@ end
 
 bm.prevReg = {-1,-1}
 
--- This (could) be done with alot of replace directives and image editing in conjunction with getRegion.
--- Very slight, un-noticable changes to the color of each tile and changing their "highlight" using directives.
 function bm.highlight()
   --[[ TODO: Figure out how to highlight without adding projectiles.
       This code is an artefact from trying to use the .animation to display
@@ -267,12 +250,10 @@ function bm.highlight()
 end
 
 --[[
-  Shows the regions in /debug, as long as the tech bar is active.
+  Shows the regions in /debug, as long as the tech bar is active and showDebug is enabled in the config.
 ]]
 function bm.showRegions()
-  -- Added a config option to disable showing the regions.
   if bm.config.showDebug and bm.active then
-    -- This didn't work if i put them together for some reason. Maybe the config just didn't reload?
     local pos = bm.activePosition
     local width, height = bm.imageBlockSize[1] / 2, bm.imageBlockSize[2] / 2
 
@@ -289,24 +270,9 @@ function bm.showRegions()
   end
 end
 
--- Didn't feel like messing with the way keybinds worked.
 function bm.altActivate()
-bm.activate(true)
+  bm.activate(true)
 end
-
-function coinExample(coins)
-  bm.queue(function()
-  local coins = coins or 100
-
-    for i=1,coins do
-      world.spawnItem("money", {tech.aimPosition()[1] + math.random(0,10), tech.aimPosition()[2] + math.random(0,8)}, math.huge)
-      i = i + 1
-    end
-
-  end)
-  return
-end
-
 
 --[[
   Creation of Keybinds for toggling and activating.
@@ -317,25 +283,35 @@ Bind.create(bm.config.toggle, bm.toggle)
 Bind.create(bm.config.activate, bm.activate)
 Bind.create(bm.config.altActivate, bm.altActivate)
 
-
--- Example of valid bind. for functions look @ lines 293 - 307
-
-bm.bind(8,2,
-{
-  primary = {
-    enabled = true,
-    func = function() coinExample(20) end
-  },
-
-  alt = {
-    enabled = true,
-    func = coinExample
-  }
-})
-
 --[[
   Load other scripts that can bind regions since the bindMenu is set up.
 ]]
 for _,v in ipairs(bm.config.scripts) do
  require(v)
 end
+
+----
+-- Example(s)
+----
+
+function coinExample(coins)
+  coins = type(coins) == "number" and coins or 100
+  bm.queue(function()
+    for i=1,coins do
+      world.spawnItem("money", {tech.aimPosition()[1] + math.random(0,10), tech.aimPosition()[2] + math.random(0,8)}, 10)
+    end
+  end)
+end
+
+-- Example of valid bind. See function `coinExample`.
+bm.bind(8,2,
+{
+  primary = {
+    enabled = true,
+    func = function() coinExample(20) end
+  },
+  alt = {
+    enabled = true,
+    func = coinExample
+  }
+})
